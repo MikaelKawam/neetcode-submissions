c5026f643e3a5af155cce425e7dea2edd2832eda
Mikael Kawam <mikaelmikaelmk@gmail.com>
2026-05-08 21:06:27 -0300

    Add: three-integer-sum - submission-40

---
+++ Data Structures & Algorithms/three-integer-sum/submission-40.cs
@@ -0,0 +1,70 @@
+public class Solution {
+    public List<List<int>> ThreeSum(int[] nums)
+{
+    var length = nums.Length;
+    for (var i = 0; i < length; i++)
+    {
+        bool swapped = false;
+        for (var j = 0; j < length - 1 - i; j++)
+        {
+            if (nums[j] > nums[j + 1])
+            {
+                var temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+                swapped = true;
+            }
+        }
+
+        if (!swapped) break;
+    }
+
+    var list = new List<List<int>>();
+    var fixated = 0;
+    int left = 1, right = length - 1;
+    while (left < right && fixated != length -2)
+    {
+        var sum = nums[fixated] + nums[left] + nums[right];
+        if (sum == 0)
+        {
+            var triplet = new List<int> { nums[fixated], nums[left], nums[right] };
+            if (!list.Any(x => x.SequenceEqual(triplet)))
+            {
+                list.Add(triplet);
+            }
+            right--;
+            left++;
+
+            if (!(left < right))
+            {
+                fixated++;
+                left = fixated + 1;
+                right = length - 1;
+            }
+        }
+        else if (sum < 0)
+        {
+            left++;
+
+            if (!(left < right))
+            {
+                fixated++;
+                left = fixated + 1;
+                right = length - 1;
+            }
+        }     
+        else
+        {
+            right--;
+            if (!(left < right))
+            {
+                fixated++;
+                left = fixated + 1;
+                right = length - 1;
+            }
+        }
+    }
+
+        return list;
+}
+}
