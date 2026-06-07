47ef4518032020108d20165ac78c3c8fc2658534
Mikael Kawam <mikaelmikaelmk@gmail.com>
2026-06-07 08:10:28 -0300

    Add: invert-a-binary-tree - submission-12

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-12.cs
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        TreeNode Inverter(TreeNode _root)
+        {
+            if (_root == null) return _root;
+
+            var temp = _root.right;
+            _root.right = _root.left;
+            _root.left = temp;
+
+            _root.right = Inverter(_root.right);
+            _root.left = Inverter(_root.left);
+
+            return _root;
+        }
+
+        return Inverter(root);
+    }
+}
