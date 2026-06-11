b2fbebbe3c3ddf776127bf2c603e0c6d50cdb9e4
Mikael Kawam <mikaelmikaelmk@gmail.com>
2026-06-11 16:55:08 -0300

    Add: level-order-traversal-of-binary-tree - submission-10

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-10.cs
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ 
+public class Solution {
+    public List<List<int>> LevelOrder(TreeNode root)
+{
+    if (root == null)
+        return new List<List<int>>();
+
+    var result = new List<List<int>>
+    {
+        new List<int> { root.val }
+    };
+
+    var left = LevelOrder(root.left);
+    var right = LevelOrder(root.right);
+
+    var commonLevels = Math.Min(left.Count, right.Count);
+
+    for (int i = 0; i < commonLevels; i++)
+    {
+        left[i].AddRange(right[i]);
+    }
+
+    if (left.Count > right.Count)
+    {
+        result.AddRange(left);
+    }
+    else
+    {
+        result.AddRange(left);
+
+        for (int i = left.Count; i < right.Count; i++)
+        {
+            result.Add(right[i]);
+        }
+    }
+
+    return result;
+}
+}
