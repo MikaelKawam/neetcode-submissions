b85a7af01c38869a9334642caf42abb27e86d3bc
Mikael Kawam <mikaelmikaelmk@gmail.com>
2026-07-18 23:01:30 -0300

    Add: is-palindrome - submission-21

---
+++ Data Structures & Algorithms/is-palindrome/submission-21.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        s = s.lower()
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+                continue
+            if not s[r].isalnum():
+                r -= 1
+                continue
+            if s[r] != s[l]:
+                return False
+                
+            r -= 1
+            l += 1
+            
+        return True
