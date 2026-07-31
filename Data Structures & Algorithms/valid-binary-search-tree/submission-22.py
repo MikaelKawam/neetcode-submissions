c567c5c6d489a3adec4a6b41115d5c9e5c976f09
Mikael Kawam <mikaelmikaelmk@gmail.com>
2026-07-30 22:01:08 -0300

    Add: valid-binary-search-tree - submission-22

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-22.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def bstValidInterval(self, root, min, max):
+        if not root: return True
+
+        if root.left:
+            if root.val <= root.left.val or root.left.val <= min:
+                return False
+
+        if root.right:
+            if root.val >= root.right.val or root.right.val >= max:
+                return False
+
+        return self.bstValidInterval(root.left, min, root.val) and self.bstValidInterval(root.right, root.val, max)
+        
+
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        min, max = float('-inf'), float('inf')
+
+        return  self.bstValidInterval(root, min, max)
+        
